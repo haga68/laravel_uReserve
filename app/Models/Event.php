9bfa4f379a6aca129aee2de8e8befd9500cea149
daisuke <gaah68@hotmail.co.jp>
2024-10-08 13:14:47 +0800

    event show

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Carbon\Carbon;
 
 class Event extends Model
 {
@@ -17,4 +19,28 @@ class Event extends Model
         'end_date',
         'is_visible'
     ];
+
+    protected function eventDate(): Attribute   // 戻り値の型
+    {
+        return new Attribute(
+            get: fn () => Carbon::parse($this->start_date)->format('Y年m月d日')// アクセサ
+        );
+    }
+
+    protected function startTime(): Attribute
+    {
+        return new Attribute(
+            get: fn () => Carbon::parse($this->start_date)->format('H時i分') 
+        );
+    }
+    
+    protected function endTime(): Attribute
+    {
+        return new Attribute(
+            get: fn () => Carbon::parse($this->end_date)->format('H時i分') 
+        );
+    }
+    
+    //　アクセサ、ミューテタ・・DBに情報保存時やDBから情報取得時にデータを加工する機能
+
 }
